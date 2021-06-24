f82af48fc47b9a0a1cf6723c6cbea50ff7b1284a
artem <artem.ermakov.com@gmail.com>
2021-06-24 07:07:09 +0500

    master

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,7 +4,7 @@ function Car(props){
     return(
         <div className="Car"> 
             {props.children}
-            <h3>Car name: {props.name}</h3>
+            <h3>Car name: <span>{props.name}</span></h3>
             <p>Year: {props.year}</p> 
             <input type="text" value={props.name} placeholder={"Car name"} onChange={props.onChangeName}/>
             <button onClick={props.onDelete}>Delete</button>
