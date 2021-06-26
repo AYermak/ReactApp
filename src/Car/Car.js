d754bfa91797455f9f7aabae709c828580d64636
artem <artem.ermakov.com@gmail.com>
2021-06-26 05:40:19 +0500

    Master

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,13 +1,17 @@
 import './Car.css';
+import { FaFolder, FaBeer  } from 'react-icons/fa';
 
 function Car(props){
     return(
         <div className="Car"> 
             {props.children}
-            <h3>Car name: <span>{props.name}</span></h3>
-            <p>Year: {props.year}</p> 
+            <h3>Car name: <span>{props.name}<FaFolder /></span></h3>
+            <p>Year: {props.year}<FaBeer /></p> 
             <input type="text" value={props.name} placeholder={"Car name"} onChange={props.onChangeName}/>
             <button onClick={props.onDelete}>Delete</button>
+            <div className={"btn-close"}><span class="material-icons-outlined">
+clear
+</span></div>
         </div>   
     )
 }
