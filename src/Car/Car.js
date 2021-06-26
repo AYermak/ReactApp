66a29efeb497bbb5e7bfeb26bbe4e02a6c8963ff
artem <artem.ermakov.com@gmail.com>
2021-06-26 06:59:15 +0500

    master

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,14 +1,21 @@
-import './Car.css';
+import 'Car/Car.css';
 import { FaFolder, FaBeer  } from 'react-icons/fa';
 import { MdCode } from 'react-icons/md';
 
 function Car(props){
+    const inputClasses = ['input'];
     return(
         <div className="Car"> 
             {props.children}
             <h3>Car name: <span>{props.name}<FaFolder /></span></h3>
             <p>Year: {props.year}<MdCode /></p> 
-            <input type="text" value={props.name} placeholder={"Car name"} onChange={props.onChangeName}/>
+            <input
+                type="text" 
+                value={props.name} 
+                placeholder={"Car name"} 
+                onChange={props.onChangeName}
+                className={inputClasses.join}
+            />
             <button onClick={props.onDelete}>Delete</button>
             <div className={"btn-close"}><span class="material-icons-outlined">
 clear
